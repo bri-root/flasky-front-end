9739d74f9909eee3d28968840058c4fec1ca4d47
Brianna Root <brianna.k.root@gmail.com>
2024-12-12 22:08:05 +1300

    Created components folder (Cat.jsx and CatList.jsx). Added CatList to App.jsx and exported app, in CatList.jsx expored CatList and imported Cat, created li in Cat and exported Cat.

---
+++ src/components/Cat.jsx
@@ -0,0 +1,14 @@
+const Cat = () => {
+    return(
+        <>
+        <li>
+            <h3> Name of Cat </h3>
+            <p>🐱</p>
+            <button>Pet Cat</button>
+        </li>
+        </>
+        
+    );
+};
+
+export default Cat;
---
+++ src/components/CatList.jsx
@@ -0,0 +1,17 @@
+import Cat from './Cat';
+const CatList = () => {
+
+    return (
+      <>
+        <h2> Cat List</h2>
+        <ul>
+          <Cat/>
+          <Cat/>
+          <Cat/>
+        </ul>
+      
+      </>
+    );
+}
+
+export default CatList;
